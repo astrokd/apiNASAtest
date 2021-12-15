7f7680bfa921eb68dba7170be7252219274e2fe7
Kevin Dreyer <kdreyerinseattle@gmail.com>
2021-12-15 14:45:20 -0800

    write json to file

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const https = require('https');
 require('dotenv').config()
 
+const fs = require('fs')
+
 const apiKey = process.env.API_KEY1
 const apiURL = process.env.API_URL_NEO_FEED
-const startDate = '2021-12-06'
-const endDate = '2021-12-07'
+const startDate = '2021-12-15'
+const endDate = '2021-12-15'
+const dateNow = Date.now()
 
 https.get(`${apiURL}?start_date=${endDate}&end_date=${startDate}&api_key=${apiKey}`, (response) => {
   let data = '';
@@ -13,7 +16,9 @@ https.get(`${apiURL}?start_date=${endDate}&end_date=${startDate}&api_key=${apiKe
   })
   response.on('end', () => {
     const obj = JSON.parse(data)
-    console.log(JSON.stringify(obj, null, 4));
+    const jsondata = JSON.stringify(obj, null, 4)
+    fs.writeFileSync(`NEO_${startDate}_${dateNow}`,jsondata)
+    console.log(" Data written to NEO file ");
   })
 })
 .on('error', (error) => {
